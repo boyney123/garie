9c9b26cdeb41606e2ad79a44364e0f4b2c9a2be0
David Boyne <davidboyne123@hotmail.co.uk>
2018-09-27 21:43:11 +0100

    Testing DO

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 # Required for garie-browsertime (needs to know where to map reports back too as its docker in docker...)
-REPORT_DIR=$(pwd)/plugins/garie-browsertime PAGESPEED_INSIGHTS_KEY=AIzaSyDyr1oIMNo-jNp8p_DG51dJH33YqF1S8do docker-compose up
+REPORT_DIR=$(pwd)/plugins/garie-browsertime DOCKER_PATH=$(which docker) PAGESPEED_INSIGHTS_KEY=AIzaSyDyr1oIMNo-jNp8p_DG51dJH33YqF1S8do docker-compose up
