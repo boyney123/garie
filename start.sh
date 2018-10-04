c993cef403ab35ad3e287d7fc45dbd23961f84ce
David Boyne <davidboyne123@hotmail.co.uk>
2018-10-04 11:53:57 +0100

    Fix for Grafana permissions

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Required for garie-browsertime (needs to know where to map reports back too as its docker in docker...)
-REPORT_DIR=$(pwd)/plugins/garie-browsertime DOCKER_PATH=$(which docker) PAGESPEED_INSIGHTS_KEY=YOUR_KEY docker-compose up
+REPORT_DIR=$(pwd)/plugins/garie-browsertime DOCKER_PATH=$(which docker) CURRENT_UID=$(id -u):$(id -g) PAGESPEED_INSIGHTS_KEY=YOUR_KEY docker-compose up
+
+
